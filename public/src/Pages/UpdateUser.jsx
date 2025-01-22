8b6413fbe3d12849305bf472443116b0ebf5835b
Mark-0392 <itsmxd03gmail.com>
2025-01-22 09:09:25 +0530

    rework

--- public/src/Pages/UpdateUser.jsx
+++ public/src/Pages/UpdateUser.jsx
@@ -12,15 +12,13 @@ export const action = async ({ request }) => {
     const response = await axios.patch('/api/v1/users/updateUser', data)
 
     toast.success(response.data.msg)
-    toast.success(
-      'Please login with your new updated email address to continue'
-    )
+    // toast.success(
+    //   'Please login with your new updated email address to continue'
+    // )
     return redirect('/login')
   } catch (error) {
     const errorMsg = error?.response?.data?.msg
-
     console.log(errorMsg)
-    return null
   }
 }
 
@@ -28,7 +26,7 @@ const UpdateUser = () => {
   return (
     <div className="max-sm:h-[calc(100%-40px)] md:h-[calc(100%-44px)] lg:h-[calc(100%-52px)] grid place-items-center bg-slate-800 lg:bg-white">
       <Form
-        method="patch"
+        method="PATCH"
         className=" w-11/12 max-w-[500px] grid grid-flow-row gap-y-5 rounded-lg  px-4 lg:mx-0 hover:shadow-lg hover:shadow-purple-400 lg:hover:shadow-lg lg:hover:shadow-fuchsia-400/50  duration-700 py-2 border"
       >
         <h1 className="text-base font-semibold text-white lg:text-black lg:text-2xl text-center  items-center font-Playfair_SemiBold ">
